3d938c3fb7332c6223b2ab6eb72bfa0d83f4a7ce
Daniel Hatley <daniel.hatley@gmail.com>
2018-05-13 03:53:06 +0000

    revert back to eb322662f41c46559ddca261baba2ece8013257c

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-console.log("This is my complex code!");
-console.log("More complex code");
-jklfjdlsk;ajfdksljfsdka;klfdskj
+console.log("This is my complex code!")
--- climbs.js
+++ climbs.js
@@ -1,2 +1,1 @@
 console.log("CLIMB!");
-console.log("Kootenai Canyon");
